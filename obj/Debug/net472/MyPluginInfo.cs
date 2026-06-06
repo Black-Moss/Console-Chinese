0913fa2e70bb7dbd5fcca8d1dd45979aa824b827
Black_Moss <kytheminecraft@outlook.com>
2026-06-07 03:47:48 +0800

    !!!

--- obj/Debug/net472/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace ConsoleScriptZH
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "ConsoleScript-ZH";
-        public const string PLUGIN_NAME = "ConsoleScript-ZH";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
